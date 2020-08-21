bf1b8c4583b335b8d4c70e28166b4ef8f98fe966
Saul Feliz <646634@gmail.com>
2020-08-20 20:59:41 -0500

    completed slicing exercises

---
+++ 05-Functions/the-none-type.py
@@ -0,0 +1,8 @@
+def subtract(a, b):
+    print(a - b)
+
+
+result = subtract(10, 3)
+
+print(result)
+print(type(result))
---
+++ 06-Strings-The-Basics/string-slicing-II-slicing-by-steps.py
@@ -0,0 +1,15 @@
+# def first_three_characters(str):
+#     return str[0:3]
+
+
+# print(first_three_characters(("dynasty")))
+
+
+# def last_five_characters(str):
+#     return str[-5:]
+
+# def is_palindrome(string):
+#     return string[::-1] == string
+
+
+# print(is_palindrome(("racecar")))
