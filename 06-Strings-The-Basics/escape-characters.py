682f73ce411afb1d42866f5ae95d33020000e520
Saul Feliz <646634@gmail.com>
2020-08-21 18:46:42 -0500

    escape chars

---
+++ 06-Strings-The-Basics/escape-characters.py
@@ -0,0 +1,5 @@
+print("testing printing \na new line")
+print("Saul said, \"this is amazing!\" ")
+
+# create raw string to ignore escapes: add r in front of quotes for raw
+print(r"file location is C:\user\folder")
