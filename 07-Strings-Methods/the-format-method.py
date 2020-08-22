05888da8a0e7f1e64c207f0d96e10abd626b8740
Saul Feliz <646634@gmail.com>
2020-08-22 15:03:39 -0500

    format method

---
+++ 07-Strings-Methods/the-format-method.py
@@ -0,0 +1,9 @@
+# name, adjective, noun
+
+mad_libs = "{name} laughed at the {adjective} {noun}."
+
+name = "Isabel"
+adjective = "funny"
+noun = "dog"
+
+print(mad_libs.format(name=name, adjective=adjective, noun=noun))
