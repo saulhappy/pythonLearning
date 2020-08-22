87a5c6d5c4a2d21a26738e5f7f1bf6ea7598ec10
Saul Feliz <646634@gmail.com>
2020-08-22 07:17:50 -0500

    find and index methods

---
+++ 07-Strings-Methods/the-find-and-index-methods.py
@@ -0,0 +1,17 @@
+print("saul is learning python".find("y"))
+
+print("saul is learning python".find("python"))
+print("saul is learning python".find("s"))
+print("saul is learning python".find("saul"))
+
+print("saul is learning python".find("z"))
+
+print("saul is learning python".find("s"))
+
+print("saul is learning python".find("l"))
+
+# start looking at index 5 of object
+print("saul is learning python".find("l", 5))
+
+
+# index works the same as find, but returns ValueError instead of -1 if it doesn't find anything.
