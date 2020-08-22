55f08b4da12359485540bd45e72c5f00840b200a
Saul Feliz <646634@gmail.com>
2020-08-22 08:41:42 -0500

    string methods II

---
+++ 07-Strings-Methods/boolean-methods-for-strings.py
@@ -0,0 +1,16 @@
+print("winter is coming".islower())
+print("winter Is coming".islower())
+print("winter is coming!!!!!$%#$".islower())
+
+print("winter is coming".istitle())
+
+print("winter is coming!!!!!$%#$".isalpha())
+print("winter is coming".isalpha())
+print("winteriscoming".isalpha())
+
+print("646".isnumeric())
+print("646-634-7594".isnumeric())
+
+print("          ".isspace())
+
+print("    4    ".isspace())
---
+++ 07-Strings-Methods/the-capitalize-title-lower-upper-and-swapcase-methods.py
@@ -0,0 +1,7 @@
+lyric = "find me someone to love"
+
+print(lyric.capitalize())
+print(lyric.title())
+print(lyric.upper())
+print("LOVE".lower())
+print("Find me soMEONe to lOVE".swapcase())
