cd7e495aaf10ca1b4cb484abf15b9644caec52b1
Saul Feliz <646634@gmail.com>
2020-08-22 15:08:34 -0500

    string literals

---
+++ 07-Strings-Methods/formatted-string-literals.py
@@ -0,0 +1,10 @@
+name = "Isabel"
+adjective = "funny"
+noun = "dog"
+
+print(f"{name} laughed at the {adjective} {noun}")
+
+text = F"{name} laughed at the {adjective} {noun}"
+print(text)
+
+print(f"2 + 2 is {2 + 2}")
